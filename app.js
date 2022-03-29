152dddcd1110aa86c48f9839bc58268f6a4930e7
binhdev-simp <dinhducbinh2k7@gmail.com>
2022-03-29 16:05:29 +0700

    add files

---
+++ app.js
@@ -0,0 +1,8 @@
+const textbox = document.querySelector('.inputItem');
+const addBtn = document.querySelector('.addItem');
+const textTodo = document.querySelector('p');
+
+textTodo.onclick =function(){
+    textTodo.style.color="red";
+}
+textTodo.style.color="red";
